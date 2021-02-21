6a0f1ade272e955ee453141b21ef9cd540c46448
Anshika15 <bansalanshika20@gmail.com>
2021-02-21 15:41:08 +0530

    991. Broken Calculator.java

---
+++ 991. Broken Calculator.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int brokenCalc(int X, int Y) {
+        
+        if(X >= Y)
+            return X-Y;
+        
+        return 1 + brokenCalc(X, (Y % 2 == 0) ? Y/2 : Y + 1);
+        
+    }
+}
