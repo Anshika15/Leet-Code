6e409f4de3402735abcda2c22fe77039c8c5527e
Anshika15 <bansalanshika20@gmail.com>
2021-03-19 12:32:42 +0530

    121. Best Time to Buy and Sell Stock

---
+++ 121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for(int i = 0; i < prices.length; i++)
+        {
+            min = Math.min(prices[i], min);
+            max = Math.max(prices[i] - min, max);
+        }
+        
+       return max; 
+    }
+}
