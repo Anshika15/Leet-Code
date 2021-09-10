bcba87ae10d973d8796e5edac28e2d64158c051a
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-09-10 12:03:58 +0530

    109. Convert Sorted List to Binary Search Tree

---
+++ 109. Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        
+        if(head == null)
+            return null;
+        if(head.next == null)
+        {
+            return new TreeNode(head.val);
+        }
+        
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null)
+        {
+           prev = slow;
+           slow = slow.next;
+           fast = fast.next.next;
+        }
+        
+        TreeNode root = new TreeNode(slow.val);
+        if(prev != null)
+            prev.next = null;
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        
+        return root;
+        
+    }
+}
