c9a64df63140c3e0324638a641ec6d2267850401
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-09-22 20:18:14 +0530

    35. Search Insert Position

---
+++ 35. Search Insert Position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+        int mid = start + (end-start)/2;
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[mid] > target)
+            end = mid-1;
+        else start = mid+1;
+       
+        }
+        return start;
+        
+    }
+}
