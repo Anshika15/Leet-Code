5be17a3ab18373c318036d0e537d264d02e67809
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-09-27 20:19:47 +0530

    894. All Possible Full Binary Trees

---
+++ 894. All Possible Full Binary Trees.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> allPossibleFBT(int n) {
+        
+        List<TreeNode> res = new ArrayList<>();
+        if(n == 1)
+        {
+            res.add(new TreeNode(0));
+            return res;
+        }
+        
+        for(int i = 1; i < n; i+=2)
+        {
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(n-i-1);
+            
+            for(TreeNode l : left)
+            {
+                for(TreeNode r : right)
+                {
+                    TreeNode node = new TreeNode(0);
+                    node.left = l;
+                    node.right = r;
+                    res.add(node);
+                }
+            }
+        }
+        return res;
+    }
+    
+}
