512d50c2c0df5d918b68691c167668ec725bec24
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-09-29 00:25:39 +0530

    768. Max Chunks To Make Sorted II

---
+++ 768. Max Chunks To Make Sorted II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        
+        int rmin[] = new int[arr.length+1];
+        rmin[arr.length] = Integer.MAX_VALUE;
+        for(int i = arr.length-1; i >= 0; i--)
+            rmin[i] = Math.min(rmin[i+1], arr[i]);
+        
+        int lmax = arr[0];
+        int c = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            lmax = Math.max(lmax, arr[i]);
+            if(lmax <= rmin[i+1])
+                c++;
+        }
+        
+        return c;
+        
+    }
+}
