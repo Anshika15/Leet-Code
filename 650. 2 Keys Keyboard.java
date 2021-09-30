0f70a56a165076507fec2134237944710342600b
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-09-30 23:20:39 +0530

    650. 2 Keys Keyboard

---
+++ 650. 2 Keys Keyboard.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minSteps(int n) {
+        
+        if(n == 1)
+		    return 0;
+		int ans = 0;
+		for(int i = 2; i*i <= n; i++)
+		{
+		    while(n%i == 0)
+		    {
+		        ans += i;
+		        n /= i;
+		    }
+		}
+		if(n != 1)
+		    ans += n;
+		return ans;
+        
+    }
+}
