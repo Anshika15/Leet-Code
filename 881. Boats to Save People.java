b4f20b14bb50a13da2e2af15ebdc024796f77be7
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-10-04 13:50:43 +0530

    881. Boats to Save People

---
+++ 881. Boats to Save People.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        Arrays.sort(people);
+        
+        int i = 0;
+        int j = people.length-1;
+        
+        while(i <= j)
+        {
+            if(people[i]+people[j] <= limit)
+                i++;
+            j--;
+        }
+        
+        return people.length - 1 - j;
+    }
+}
