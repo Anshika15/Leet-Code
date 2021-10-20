2597762691ea84a0d540b2bc0ba16c660fc8052f
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-10-20 07:19:08 +0530

    151. Reverse Words in a String

---
+++ 151. Reverse Words in a String.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String reverseWords(String s) {
+        
+        String[] arr = s.split("\\s+");
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length-1; i >= 0; i--)
+        {
+            sb.append(arr[i]);
+            if(i != 0)
+                sb.append(" ");
+        }
+        
+        return sb.toString().trim();
+        
+    }
+}
