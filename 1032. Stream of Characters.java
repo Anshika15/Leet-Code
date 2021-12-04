9498b12aad84f69bd467526e943e1625c860ab6a
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2021-12-04 19:31:19 +0530

    1032. Stream of Characters

---
+++ 1032. Stream of Characters.java
@@ -0,0 +1,55 @@
+class StreamChecker {
+    
+    class Node{
+        /* suffix trie */
+        Node[] childs = new Node[26];
+        boolean isEnd = false;
+    }
+    
+    private final Node root;
+    private final StringBuilder sb;
+
+    public StreamChecker(String[] words)
+    {
+        root = new Node();
+        sb = new StringBuilder();
+        
+        for(String s : words)
+        {
+            Node curr = root;
+            for(int i = s.length()-1; i >= 0; i--)
+            {
+                char ch = s.charAt(i);
+                if(curr.childs[ch-'a'] == null)
+                {
+                    curr.childs[ch-'a'] = new Node();
+                }
+                curr = curr.childs[ch-'a'];
+            }
+            
+            curr.isEnd = true;
+        }
+    }
+
+    public boolean query(char letter) {
+        
+        sb.append(letter);
+        Node curr = root;
+        for(int i = sb.length()-1; i >= 0; i--)
+        {
+            char ch = sb.charAt(i);
+            curr = curr.childs[ch-'a'];
+            if(curr == null)
+                return false;
+            if(curr.isEnd == true)
+                return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker obj = new StreamChecker(words);
+ * boolean param_1 = obj.query(letter);
+ */
