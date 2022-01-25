a1df1fcdb2512bafc1bfcb79439866c97b577c06
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2022-01-25 09:53:36 +0530

    941. Valid Mountain Array

---
+++ 941. Valid Mountain Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        
+        int i = 0;
+        while(i < arr.length-1 && arr[i] < arr[i+1])
+            i++;
+        if(i == 0 || i == arr.length-1 || arr[i] == arr[i+1])
+            return false;
+        while(i < arr.length-1 && arr[i] > arr[i+1])
+            i++;
+        return i == arr.length - 1;
+        
+    }
+}
