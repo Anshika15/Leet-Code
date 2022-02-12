bb1ac0cabe1e952af7c98e092be715ecd0651806
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2022-02-12 20:16:43 +0530

    45. Jump Game II

---
+++ 45. Jump Game II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        Integer[] dp = new Integer[nums.length];
+        dp[nums.length - 1] = 0;
+        
+        for(int i = dp.length-2; i >= 0; i--)
+        {
+            int min = Integer.MAX_VALUE;
+            int steps = nums[i];
+            for(int j = 1; j <= steps && i+j < dp.length; j++)
+            {
+                if(dp[i+j] != null && dp[i+j] < min)
+                    min = dp[i+j];
+            }
+            if(min != Integer.MAX_VALUE)
+                dp[i] = min+1;
+        }
+        return dp[0];
+        
+    }
+}
