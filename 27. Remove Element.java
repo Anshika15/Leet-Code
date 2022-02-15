e6f057ed0693e79db0cdc50401aff3ac1eb80093
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2022-02-15 17:53:19 +0530

    27. Remove Element

---
+++ 27. Remove Element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int idx = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] != val)
+            {
+                nums[idx++] = nums[i];
+            }
+        }
+        return idx;
+    }
+}
