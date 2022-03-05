a701ac708b3391b33a7a4c23f5e9aded094676da
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2022-03-05 22:01:29 +0530

    867. Transpose Matrix

---
+++ 867. Transpose Matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        
+        int[][] result = new int[matrix[0].length][matrix.length];
+    
+        for(int i = 0; i < matrix[0].length; i++)
+        {
+            for(int j = 0; j < matrix.length; j++)
+            {
+                result[i][j] = matrix[j][i];
+            }
+        }
+        return result;
+        
+    }
+}
