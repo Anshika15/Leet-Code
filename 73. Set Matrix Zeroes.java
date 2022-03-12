d5f17ec4f69c55e4e512131e52f5d7ddd0a6029a
Anshika Bansal <54213105+Anshika15@users.noreply.github.com>
2022-03-12 16:19:21 +0530

    73. Set Matrix Zeroes

---
+++ 73. Set Matrix Zeroes.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstRow = false;
+        boolean firstCol = false;
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for(int j = 0; j < matrix[i].length; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    if(i == 0) firstRow = true;
+                    if(j == 0) firstCol = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if(firstRow) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(firstCol) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
